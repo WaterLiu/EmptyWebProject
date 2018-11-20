9490bb267114d8ed33b2c13df42c3aa0cc8cbdaa
WaterLiu <water1984609@hotmail.com>
2018-11-20 15:11:55 +0800

    添加 CleanWebpackPulugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
     output: {
-        path: path.resolve(__dirname, './dist'), // 输出的路径
-        filename: 'bundle.js'  // 打包后文件
+        path: path.resolve(__dirname, './dist'),    // 输出的路径
+        filename: 'bundle.js',                      // 打包后文件
+        publicPath: '/',
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
+    devtool: 'inline-source-map',                   //source map
+    devServer: {                                    //webpack devserver
+        contentBase: './dist'                       //发布文件位置
     },
     module: {
         rules: [
@@ -25,6 +28,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({template: './src/index.html'})
     ]
 }
