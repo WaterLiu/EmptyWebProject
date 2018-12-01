8426edab18f0e81d19513add69b7a69b030ac6d6
WaterLiu <water609@qq.com>
2018-12-01 23:43:57 +0800

    no message

--- src/pages/firstPage/index.js
+++ src/pages/firstPage/index.js
@@ -1,20 +1,64 @@
 import React from 'react';
 import styles from './index.css';
+import * as mobx from 'mobx';
+import {observer} from 'mobx-react';
+import DevTools from 'mobx-react-devtools'
 
+@observer
 class FirstPage extends React.Component {
+
+    @mobx.observable data = {
+        frame: {
+            x: 10,
+            y: 20,
+            width: 100,
+            height: 100,
+        }
+    };
 
     constructor(props) {
         super(props);
 
     }
 
+    button1Clicked() {
+        mobx.remove(this.data.frame, 'x');
+    }
+
+    button2Clicked() {
+        mobx.set(this.data.frame, {x : 100});
+    }
+
     render() {
+
+        if (mobx.has(this.data.frame, 'x')) {
+            console.log('x 存在 render 刷新');
+        } else {
+            console.log('x 不存在 render 刷新');
+        }
+
         return (
-            <div>
-                <div className={styles.box}></div>
+            <div className={styles.box}>
+                <button onClick={this.button1Clicked.bind(this)}>Button1</button>
+                <button onClick={this.button2Clicked.bind(this)}>Button2</button>
+                <div>{ this.data.frame.x }</div>
+                <DevTools/>
             </div>
         );
     }
+}
+
+
+class Data {
+
+
+
+    constructor(props) {
+
+    }
+
+
+
 }
 
 export default FirstPage;
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: "development",
     entry: path.resolve(__dirname, './src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
     output: {
         path: path.resolve(__dirname, './dist'),    // 输出的路径
@@ -21,9 +22,10 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['es2015', 'react', 'stage-0'],
+                        plugins: ['transform-decorators-legacy']
                     }
                 },
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
@@ -33,6 +35,6 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({template: './src/index.html'})
+        new HtmlWebpackPlugin({template: './src/index.html'}),
     ]
 }
